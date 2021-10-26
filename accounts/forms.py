916b84bdbe9568c56db446c8e831341b1c730b13
Lee Hyun Jun <mikey1000@naver.com>
2021-10-26 19:24:58 +0900

    custom UserCreationForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,23 @@ from .models import User
 from django import forms
 
 class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs): # super()로 오버라이딩받아서 수정
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True # 필수값으로 설정
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+
+
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ['username','email','first_name','last_name']
-    pass
+
+    # email 중복 막기
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email:
+            qs = User.objects.filter(email=email)
+            if qs.exists():
+                raise forms.ValidationError("이미 등록된 Email입니다")
+        return email
+
