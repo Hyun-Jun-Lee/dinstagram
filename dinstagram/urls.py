f37a0ef3fa1f5caa03e5d7fb95140296aa5caaba
Lee Hyun Jun <mikey1000@naver.com>
2021-10-28 16:46:49 +0900

    add avatar

--- dinstagram/urls.py
+++ dinstagram/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django_pydenticon.views import image as pydenticon_image
 
 
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', login_required(TemplateView.as_view(template_name='root.html')), name='root'),
     path('accounts/', include('accounts.urls')),
+    path('identicon/image/<path:data>', pydenticon_image, name='pydenticon_image'),
 ]
 
 if settings.DEBUG:
