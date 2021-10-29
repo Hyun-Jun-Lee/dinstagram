eae88473e59c50003c64e0094b9a21cbf3543360
Lee Hyun Jun <mikey1000@naver.com>
2021-10-29 22:48:44 +0900

    add password_change path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.urls import path
-
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from accounts import views
 
 urlpatterns = [
@@ -9,5 +9,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('edit/', views.profile_edit, name='profile_edit'),
+    path('password_change/', views.Password_Change_View.as_view(), name='password_change'),
 ]
 
