09360448ecdf7ad3917ad9020e68944693719bfb
Lee Hyun Jun <mikey1000@naver.com>
2021-11-04 22:24:43 +0900

    comment

--- instagram/urls.py
+++ instagram/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     re_path(r'^(?P<username>[\w.@+-]+)/$', views.user_page, name='user_page'),
     path('post/<int:pk>/like/', views.post_like, name='post_like'),
     path('post/<int:pk>/unlike/', views.post_unlike, name='post_unlike'),
-]
+    # comment의 pk와 혼동되지 않게 post_pk라고 따로 지정
+    path('post/<int:post_pk>/comment/new/', views.comment_new, name='comment_new'),
+    ]
